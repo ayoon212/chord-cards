49f2aaf2844bc0a0e92370522377e3bef5a7f97c
Andy Yoon <ayoon212@gmail.com>
2020-07-04 13:12:37 -0700

    Add difficulty switching with basic styles

--- components/card-setup.js
+++ components/card-setup.js
@@ -1,13 +1,36 @@
-import { useState } from 'react';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+
+const buttonStyle = css`
+
+`;
+const activeButtonStyle = css`
+  background-color: dodgerblue;
+  color: white;
+`;
+
+const buttons = [
+  { value: 'easy', label: 'Easy' },
+  { value: 'medium', label: 'Medium' },
+  { value: 'hard', label: 'Hard' },
+  { value: 'custom', label: 'Custom' }
+]
 
 export const CardSetup = ({ difficulty, setDifficulty, startCardPlay }) => (
   <section>
     <h2>Card Setup</h2>
-    <h3>Difficulty</h3>
-    <button onClick={setDifficulty('easy')}>Easy</button>
-    <button onClick={setDifficulty('easy')}>Medium</button>
-    <button onClick={setDifficulty('easy')}>Hard</button>
-    <button onClick={setDifficulty('easy')}>Custom</button>
+    <div>
+      <h3>Difficulty</h3>
+      {buttons.map(button => (
+        <button
+          key={button.value}
+          onClick={() => setDifficulty(button.value)}
+          css={[
+            buttonStyle,
+            difficulty === button.value ? activeButtonStyle : null
+          ]}>{button.label}</button>
+      ))}
+    </div>
     <button onClick={startCardPlay}>Start!</button>
   </section>
 );
